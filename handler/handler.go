e866cfd8227c315067ea41530abb511f02420cdb
Camilo Crespo <crezam@live.com>
2017-01-27 23:50:02 -0500

    Fix import

--- handler/handler.go
+++ handler/handler.go
@@ -1,8 +1,11 @@
 package handler
 
+import "github.com/crezam/actions-on-google-golang/model"
+
+
 type ApiAiAssistant struct {
 }
 
-func (*ApiAiAssistant) handle(request ApiAiRequest) error {
+func (*ApiAiAssistant) handle(request model.ApiAiRequest) error {
 	return nil
 }
