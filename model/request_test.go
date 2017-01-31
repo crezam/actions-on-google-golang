2093b1c3083706934859d243f456f250cd548cdf
Camilo Crespo <crezam@live.com>
2017-01-30 23:54:17 -0500

    Start of test framework

--- model/request_test.go
+++ model/request_test.go
@@ -1,3 +1,25 @@
 package model_test
 
+import (
+	"encoding/json"
+	"github.com/crezam/actions-on-google-golang/model"
+	"os"
+	"testing"
+)
 
+func TestRequestParsing(t *testing.T) {
+
+	var req model.ApiAiRequest
+
+	file, _ := os.Open("./data/sample_request1.json")
+	dec := json.NewDecoder(file)
+
+	if err := dec.Decode(&req); err != nil {
+		t.Fatal(err)
+	}
+
+	if resolvedQuery := req.Result.ResolvedQuery; resolvedQuery != "Hi, my name is Sam!" {
+		t.Fatal("Failed parsing resolved query")
+	}
+
+}
