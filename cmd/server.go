399566e7c424eff326728dd6b40b265e24ee7bee
Camilo Crespo <crezam@live.com>
2017-02-16 23:48:56 -0500

    A primitive webhook server for API-API requests

---
+++ cmd/server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/crezam/actions-on-google-golang/model"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/webhook", HandleWebhook)
+	http.ListenAndServe("localhost:8000", nil)
+}
+
+func HandleWebhook(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	defer r.Body.Close()
+	var webhookRequest model.ApiAiRequest
+	err := decoder.Decode(&webhookRequest)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(webhookRequest.Result.ResolvedQuery)
+	w.Write([]byte("OK"))
+}
