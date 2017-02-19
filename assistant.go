6658ef3eeda1cced1c88709ccb110f98eaaf3fd0
Camilo Crespo <crezam@live.com>
2017-02-18 23:51:48 -0500

    Restructure for placing ApiAiAssistant

---
+++ assistant.go
@@ -0,0 +1,6 @@
+package actions_on_google_golang
+
+type ApiAiAssistant struct {
+}
+
+
--- handler/handler.go
+++ handler/handler.go
@@ -1,11 +1,1 @@
 package handler
-
-import "github.com/crezam/actions-on-google-golang/model"
-
-
-type ApiAiAssistant struct {
-}
-
-func (*ApiAiAssistant) handle(request model.ApiAiRequest) error {
-	return nil
-}
